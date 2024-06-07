369a9237c0e9b265dbc17c301bb4adfade810f3e
Ener Octavio Buitrago Camelo <49449065+Octaviusb@users.noreply.github.com>
2024-06-07 18:00:36 -0500

    primer commit

--- aplicacion/ecommerce/src/App.jsx
+++ aplicacion/ecommerce/src/App.jsx
@@ -1,10 +1,14 @@
-function App() {
+/* eslint-disable no-unused-vars */
+import React from 'react';
 
+const API_URL = import.meta.env.VITE_API_URL;
+
+function App() {
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <h1>Hola {API_URL}</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
